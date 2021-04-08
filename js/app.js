40fbd7689a7557b1b9dbe03c7dbf40b8f68a0edc
Stephen Chris <stephenchris91@gmail.com>
2021-04-07 17:16:12 -0700

    completed the infinite scroll project

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Unsplash API
-const count = 20;
+const count = 10;
 const apiKey= 'brXEP8iJHpzp9Hv2Io3jXEY3Z1iC8txErjTLdd78vw0'
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -10,10 +10,25 @@ const imageContainer = document.querySelector('#img-container')
 const loader = document.querySelector('.loader')
 
 //create a global array to store the data from the API
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+//check if images are loaded
+
+function imageLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 ////create elements for each element in the array and add it to them
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // Run a function for each element in the Array
     //template of the element using template literals
     photosArray.forEach((photo) => {
@@ -28,10 +43,11 @@ function displayPhotos(){
         img.setAttribute('alt', photo.alt_description);
         img.setAttribute('title', photo.alt_description);
 
+
+        img.addEventListener('load', imageLoaded)
         item.appendChild(img);
         imageContainer.appendChild(item)
 
-        console.log(imageContainer)
     });
 }
 
@@ -41,7 +57,6 @@ async function getPhotos(){
     try {
         const response = await fetch(apiURL)
         photosArray = await response.json();
-        console.log(photosArray)
         displayPhotos();
     } catch (error) {
         //catch the error here
@@ -50,5 +65,14 @@ async function getPhotos(){
     }
 }
 
+
+//check for scroll event
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos()
+    }
+})
 //onload 
 getPhotos()
